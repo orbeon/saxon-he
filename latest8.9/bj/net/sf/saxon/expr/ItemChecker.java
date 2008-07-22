6522b7d31fe50475030d50396ec78383710a55f2
Michael Kay <mike@saxonica.com>
2008-07-22 03:00:08 +0000

    Fix bug 2024143 (item checking in push mode)

--- latest8.9/bj/net/sf/saxon/expr/ItemChecker.java
+++ latest8.9/bj/net/sf/saxon/expr/ItemChecker.java
@@ -174,7 +174,7 @@ public final class ItemChecker extends UnaryExpression {
             context.setReceiver(filter);
             next.process(context);
             filter.close();
-            context.setReceiver(filter);
+            context.setReceiver(out);
         } else {
             super.process(context);
         }
