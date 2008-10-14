83e96fb74ca977979e70d41a3ebae13862ee9de8
Michael Kay <mike@saxonica.com>
2008-10-14 18:58:50 +0000

    Version numbering change to prepare for release of 9.1.0.3

--- latest9.1/bj/net/sf/saxon/Version.java
+++ latest9.1/bj/net/sf/saxon/Version.java
@@ -6,10 +6,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9,1,0,2};
-    private static final String VERSION = "9.1.0.2";
-    private static final String BUILD = "090117"; //mmddhh
-    private static final String RELEASE_DATE = "2008-09-02";
+    private static final int[] STRUCTURED_VERSION = {9,1,0,3};
+    private static final String VERSION = "9.1.0.3";
+    private static final String BUILD = "101520"; //mmddhh
+    private static final String RELEASE_DATE = "2008-10-15";
 
     private Version() {
         // class is never instantiated
