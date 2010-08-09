22cbe1f8d757927c9d816f152bc43d8e2fed4c99
Michael Kay <mike@saxonica.com>
2010-08-09 10:10:13 +0000

    Update build files for 9.2.1.2 maintenance release

--- latest9.2/hen/csource/api/Saxon.Api/Properties/AssemblyInfo.cs
+++ latest9.2/hen/csource/api/Saxon.Api/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@
           [assembly: AssemblyCulture("")]
           [assembly: ComVisible(false)]
           [assembly: Guid("8cf9aad8-da5f-47f0-9251-1fbcbdcfe477")]
-          [assembly: AssemblyVersion("9.2.1.1")]
-          [assembly: AssemblyFileVersion("9.2.1.1")]
+          [assembly: AssemblyVersion("9.2.1.2")]
+          [assembly: AssemblyFileVersion("9.2.1.2")]
         
