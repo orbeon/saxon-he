f4357ff52298cb40e93be6e6d1c9bb0e822a335b
O'Neil Delpratt <oneil@saxonica.com>
2011-12-14 16:06:47 +0000

    hen folder

---
+++ latest9.4/csource/cmd/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+
+		  using System.Reflection;
+          using System.Runtime.CompilerServices;
+          using System.Runtime.InteropServices;
+          [assembly: AssemblyCompany("Saxonica")]
+          [assembly: AssemblyProduct("Saxon.EE")]
+          [assembly: AssemblyCopyright("Copyright © Saxonica 2011")]
+          [assembly: AssemblyVersion("9.4.0.1")]
+          [assembly: AssemblyFileVersion("9.4.0.1")]
+        
