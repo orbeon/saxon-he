4ec3bc633cf6729ff6e2a0eeb33aea7cec81e80a
Michael Kay <mike@saxonica.com>
2012-01-20 17:52:34 +0000

    Update 9.4.0.2 version build date

--- latest9.4/hej/net/sf/saxon/Version.java
+++ latest9.4/hej/net/sf/saxon/Version.java
@@ -8,8 +8,8 @@ public final class Version {
 
     private static final int[] STRUCTURED_VERSION = {9,4,0,2};
     private static final String VERSION = "9.4.0.2";
-    private static final String BUILD = "011610"; //mmddhh
-    private static final String RELEASE_DATE = "2012-01-16";
+    private static final String BUILD = "012018"; //mmddhh
+    private static final String RELEASE_DATE = "2012-01-20";
     private static final String MAJOR_RELEASE_DATE = "2011-12-09";
 
     private Version() {
