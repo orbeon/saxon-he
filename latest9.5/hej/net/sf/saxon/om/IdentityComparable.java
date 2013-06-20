ea9707b56297cdeab49a7b61021c3bb14c249ab7
O'Neil Delpratt <oneil@saxonica.com>
2013-06-20 15:14:03 +0000

    Update to comments in IdentityComparable

--- latest9.5/hej/net/sf/saxon/om/IdentityComparable.java
+++ latest9.5/hej/net/sf/saxon/om/IdentityComparable.java
@@ -1,6 +1,10 @@
 package net.sf.saxon.om;
 
-
+/**
+ * The IdentityComparable class provides a way to compare class objects for equality
+ *
+ *
+ */
 public interface IdentityComparable {
 
     public boolean isIdentical(IdentityComparable other);
