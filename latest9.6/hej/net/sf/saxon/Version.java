8438c81064540b845c97955b889f29b2a3177bd1
O'Neil Delpratt <oneil@saxonica.com>
2015-03-25 11:33:31 +0000

    Update to Saxon version number: 9.6.0.5 - Maintenance release

--- latest9.6/hej/net/sf/saxon/Version.java
+++ latest9.6/hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 6, 0, 4};
-    private static final String VERSION = "9.6.0.4";
-    private static final String BUILD = "0113415"; //mmddhh
-    private static final String RELEASE_DATE = "2015-01-14";
+    private static final int[] STRUCTURED_VERSION = {9, 6, 0, 5};
+    private static final String VERSION = "9.6.0.5";
+    private static final String BUILD = "0325415"; //mmddhh
+    private static final String RELEASE_DATE = "2015-03-25";
     private static final String MAJOR_RELEASE_DATE = "2014-10-02";
 
     private Version() {
