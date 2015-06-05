3aa38043e047eaca621ab3106b1bca99e70a5a58
O'Neil Delpratt <oneil@saxonica.com>
2015-06-05 14:58:30 +0000

    Update date in Version class

--- latest9.6/hej/net/sf/saxon/Version.java
+++ latest9.6/hej/net/sf/saxon/Version.java
@@ -15,8 +15,8 @@ public final class Version {
 
     private static final int[] STRUCTURED_VERSION = {9, 6, 0, 5};
     private static final String VERSION = "9.6.0.6";
-    private static final String BUILD = "0604415"; //mmddhh
-    private static final String RELEASE_DATE = "2015-06-04";
+    private static final String BUILD = "0605415"; //mmddhh
+    private static final String RELEASE_DATE = "2015-06-05";
     private static final String MAJOR_RELEASE_DATE = "2014-10-02";
 
     private Version() {
