36a2dad92cb79e7c878338923d03ca4dc7521f76
O'Neil Delpratt <oneil@saxonica.com>
2015-08-03 10:39:22 +0000

    Update to the Java Version number for the maintenance release: 9.6.0.7

--- latest9.6/hej/net/sf/saxon/Version.java
+++ latest9.6/hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 6, 0, 5};
-    private static final String VERSION = "9.6.0.6";
-    private static final String BUILD = "0605415"; //mmddhh
-    private static final String RELEASE_DATE = "2015-06-05";
+    private static final int[] STRUCTURED_VERSION = {9, 6, 0, 7};
+    private static final String VERSION = "9.6.0.7";
+    private static final String BUILD = "080412"; //mmddhh
+    private static final String RELEASE_DATE = "2015-08-04";
     private static final String MAJOR_RELEASE_DATE = "2014-10-02";
 
     private Version() {
