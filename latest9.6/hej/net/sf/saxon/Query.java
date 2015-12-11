728568a5f8bff7700adf5577e5cc8a0a1e728cd6
Michael Kay <mike@saxonica.com>
2015-12-11 15:15:21 +0000

    Fix bug 2532 - Query initialization hook

--- latest9.6/hej/net/sf/saxon/Query.java
+++ latest9.6/hej/net/sf/saxon/Query.java
@@ -266,13 +266,13 @@ public class Query {
         //config.setHostLanguage(Configuration.XQUERY);
         processor = new Processor(config);
 
-        XQueryCompiler compiler = processor.newXQueryCompiler();
-        compiler.setSchemaAware(schemaAware);
-
         // Check the command-line arguments.
 
         try {
             parseOptions(options);
+
+            XQueryCompiler compiler = processor.newXQueryCompiler();
+            compiler.setSchemaAware(schemaAware);
 
             if (languageVersion != null) {
                 compiler.setLanguageVersion(languageVersion);
