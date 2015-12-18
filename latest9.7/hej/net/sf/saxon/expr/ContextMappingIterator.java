310d2f332b1764ce5479bc28eca9f49b9579f31c
Michael Kay <mike@saxonica.com>
2015-12-18 23:19:57 +0000

    Fix bug #2549 - closing ContextMappingIterator

--- latest9.7/hej/net/sf/saxon/expr/ContextMappingIterator.java
+++ latest9.7/hej/net/sf/saxon/expr/ContextMappingIterator.java
@@ -77,6 +77,9 @@ public final class ContextMappingIterator<T extends Item> implements SequenceIte
 
     public void close() {
         base.close();
+        if (stepIterator != null) {
+            stepIterator.close();
+        }
     }
 
     /*@NotNull*/
