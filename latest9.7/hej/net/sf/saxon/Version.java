31ca1f949812367f9413eb24c3fada1f1ac70336
O'Neil Delpratt <oneil@saxonica.com>
2016-01-14 10:27:31 +0000

    Update 9.7 maintenance version to 9.7.0.2

--- latest9.7/hej/net/sf/saxon/Version.java
+++ latest9.7/hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 7, 0, 1};
-    private static final String VERSION = "9.7.0.1";
-    private static final String BUILD = "112715"; //mmddhh
-    private static final String RELEASE_DATE = "2015-11-27";
+    private static final int[] STRUCTURED_VERSION = {9, 7, 0, 2};
+    private static final String VERSION = "9.7.0.2";
+    private static final String BUILD = "011415"; //mmddhh
+    private static final String RELEASE_DATE = "2016-01-14";
     private static final String MAJOR_RELEASE_DATE = "2015-11-27";
 
     private Version() {
