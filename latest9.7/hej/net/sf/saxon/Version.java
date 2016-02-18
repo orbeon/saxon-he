3137152ca5e1c5b7c37cb59f449331c9f93a5d05
O'Neil Delpratt <oneil@saxonica.com>
2016-02-18 14:59:19 +0000

    Update to the date on the 9.7 Version

--- latest9.7/hej/net/sf/saxon/Version.java
+++ latest9.7/hej/net/sf/saxon/Version.java
@@ -15,8 +15,8 @@ public final class Version {
 
     private static final int[] STRUCTURED_VERSION = {9, 7, 0, 3};
     private static final String VERSION = "9.7.0.3";
-    private static final String BUILD = "021612"; //mmddhh
-    private static final String RELEASE_DATE = "2016-02-16";
+    private static final String BUILD = "021816"; //mmddhh
+    private static final String RELEASE_DATE = "2016-02-18";
     private static final String MAJOR_RELEASE_DATE = "2015-11-27";
 
     private Version() {
