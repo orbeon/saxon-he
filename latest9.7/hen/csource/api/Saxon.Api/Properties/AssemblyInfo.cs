22659a26341d8691d2a6766fe3dc4b23226d0168
O'Neil Delpratt <oneil@saxonica.com>
2016-03-11 18:01:16 +0000

    Update the Saxon .NET file

--- latest9.7/hen/csource/api/Saxon.Api/Properties/AssemblyInfo.cs
+++ latest9.7/hen/csource/api/Saxon.Api/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Saxonica")]
 [assembly: AssemblyProduct("Saxon.Api")]
-[assembly: AssemblyCopyright("Copyright © Saxonica 2009")]
+[assembly: AssemblyCopyright("Copyright © Saxonica 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.7.0.1")]
-[assembly: AssemblyFileVersion("9.7.0.1")]
+[assembly: AssemblyVersion("9.7.0.4")]
+[assembly: AssemblyFileVersion("9.7.0.4")]
