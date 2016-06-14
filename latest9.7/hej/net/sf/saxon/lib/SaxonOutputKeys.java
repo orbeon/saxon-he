36d45f1ed7bb5742c2dfc8b4592e8f109edc7e7e
Michael Kay <mike@saxonica.com>
2016-06-14 21:53:50 +0000

    Fix bug 2795: cdata-section-elements

--- latest9.7/hej/net/sf/saxon/lib/SaxonOutputKeys.java
+++ latest9.7/hej/net/sf/saxon/lib/SaxonOutputKeys.java
@@ -507,7 +507,7 @@ public class SaxonOutputKeys {
             return false;
         }
         int closer = value.indexOf('}');
-        return closer >= 2 &&
+        return closer >= 1 &&
                 closer != value.length() - 1 &&
                 NameChecker.isValidNCName(value.substring(closer + 1));
     }
