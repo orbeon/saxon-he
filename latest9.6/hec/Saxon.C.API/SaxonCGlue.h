0c35adb7614c2621169a790e71905e2aa6757286
O'Neil Delpratt <oneil@saxonica.com>
2016-07-14 09:48:10 +0000

    Saxon/C workaround for boolean typeof

--- latest9.6/hec/Saxon.C.API/SaxonCGlue.h
+++ latest9.6/hec/Saxon.C.API/SaxonCGlue.h
@@ -33,17 +33,17 @@
 
 #ifndef __cplusplus
 #ifndef _BOOL
-typedef unsigned char bool;
-static const bool false = 0;
-static const bool true = 1;
+typedef unsigned char booli;
+static const booli __false = 0;
+static const booli __true = 1;
 #endif
 #endif
 
 #ifndef __cplusplus 
 	#if defined(LICENSE)
-		#define license true;
+		#define license __true;
 	#else
-		#define license false
+		#define license __false
 	#endif
 #endif
 
