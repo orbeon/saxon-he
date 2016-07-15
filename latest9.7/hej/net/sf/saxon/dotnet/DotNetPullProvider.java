da507ec580fb15b594b74eeca5310513bea4415e
O'Neil Delpratt <oneil@saxonica.com>
2016-07-15 10:31:19 +0000

    Update to DotNetPullProvider conflicts

--- latest9.7/hej/net/sf/saxon/dotnet/DotNetPullProvider.java
+++ latest9.7/hej/net/sf/saxon/dotnet/DotNetPullProvider.java
@@ -216,13 +216,11 @@ public class DotNetPullProvider implements PullProvider, Location {
      */
 
     public Location getSourceLocator() {
-	if (parser instanceof IXmlLineInfo || parser instanceof XmlValidatingReader) {
-        if (parser instanceof IXmlLineInfo || parser instanceof XmlValidatingReader) {
+	    if (parser instanceof IXmlLineInfo || parser instanceof XmlValidatingReader) {
             return this;
         } else {
             return null;
         }
-		//return this;
     }
 
     /**
