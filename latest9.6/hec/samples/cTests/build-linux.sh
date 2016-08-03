bffd220e7a83caa1dfb01acdde670a43cafb9bb3
O'Neil Delpratt <oneil@saxonica.com>
2016-08-03 13:08:58 +0000

    Update to the build script

--- latest9.6/hec/samples/cTests/build-linux.sh
+++ latest9.6/hec/samples/cTests/build-linux.sh
@@ -6,10 +6,14 @@
 
 #jc =p MyDll.prj
 #rm -rf MyDll_jetpdb
+export JET_HOME=/usr/lib/rt
+export PATH=$JET_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$JET_HOME/lib/x86/shared:$LD_LIBRARY_PATH
 
-gcc -m32 -I$jdkdir/include -I$jdkdir/include/linux  ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c ../../Saxon.C.API/SaxonCXPath.c  testXSLT.c -o testXSLT -ldl -lc $1
 
-gcc -m32 -I$jdkdir/include -I$jdkdir/include/linux  ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c  testXQuery.c -o testXQuery -ldl -lc $1
+gcc -m32 ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c ../../Saxon.C.API/SaxonCXPath.c  testXSLT.c -o testXSLT -ldl -lc $1
 
-gcc -m32 -I$jdkdir/include -I$jdkdir/include/linux  ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c ../../Saxon.C.API/SaxonCXPath.c testXPath.c -o testXPath -ldl -lc $1
+gcc -m32  ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c  testXQuery.c -o testXQuery -ldl -lc $1
+
+gcc -m32  ../../Saxon.C.API/SaxonCGlue.c ../../Saxon.C.API/SaxonCProcessor.c ../../Saxon.C.API/SaxonCXPath.c testXPath.c -o testXPath -ldl -lc $1
 
