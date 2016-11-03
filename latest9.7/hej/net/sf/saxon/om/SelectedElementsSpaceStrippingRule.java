9482a7e769de2b4122510a1832424e90e338780c
Michael Kay <mike@saxonica.com>
2016-11-03 14:51:30 +0000

    Fix bug 3015

--- latest9.7/hej/net/sf/saxon/om/SelectedElementsSpaceStrippingRule.java
+++ latest9.7/hej/net/sf/saxon/om/SelectedElementsSpaceStrippingRule.java
@@ -33,7 +33,7 @@ public class SelectedElementsSpaceStrippingRule implements SpaceStrippingRule {
 
     private Rule anyElementRule = null;
     private Rule unnamedElementRuleChain = null;
-    private HashMap<NodeName, Rule> namedElementRules = null;
+    private HashMap<NodeName, Rule> namedElementRules = new HashMap<NodeName, Rule>(32);
     private int sequence = 0;
     private boolean rejectDuplicates; // in XSLT 3.0, duplicate conflicting rules are a static error
 
@@ -108,9 +108,6 @@ public class SelectedElementsSpaceStrippingRule implements SpaceStrippingRule {
             newRule.setAlwaysMatches(true);
             int fp = test.getFingerprint();
             CodedName key = new CodedName(fp, ((NameTest) test).getNamePool());
-            if (namedElementRules == null) {
-                namedElementRules = new HashMap<NodeName, Rule>(32);
-            }
             Rule chain = namedElementRules.get(key);
             namedElementRules.put(key, addRuleToList(newRule, chain, true));
         } else {
