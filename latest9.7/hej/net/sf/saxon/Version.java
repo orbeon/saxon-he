d5d3bf95f23a5a1315b4a729686d19a4b2808b30
O'Neil Delpratt <oneil@saxonica.com>
2016-11-22 12:38:45 +0000

    Update Saxxon version number: 9.7.0.12

--- latest9.7/hej/net/sf/saxon/Version.java
+++ latest9.7/hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 7, 0, 11};
-    private static final String VERSION = "9.7.0.11";
-    private static final String BUILD = "110216"; //mmddhh
-    private static final String RELEASE_DATE = "2016-11-02";
+    private static final int[] STRUCTURED_VERSION = {9, 7, 0, 12};
+    private static final String VERSION = "9.7.0.12";
+    private static final String BUILD = "112212"; //mmddhh
+    private static final String RELEASE_DATE = "2016-11-22";
     private static final String MAJOR_RELEASE_DATE = "2015-11-27";
 
     private Version() {
