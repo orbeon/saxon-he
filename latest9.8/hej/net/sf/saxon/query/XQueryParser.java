90f2f38b018dc86be07e2294f3aaec6a9f892dee
Michael Kay <mike@saxonica.com>
2018-10-12 15:37:14 +0000

    Fix bug 3970 (XQuery parsing document{}, try{}, catch{})

--- latest9.8/hej/net/sf/saxon/query/XQueryParser.java
+++ latest9.8/hej/net/sf/saxon/query/XQueryParser.java
@@ -3482,7 +3482,12 @@ public class XQueryParser extends XPathParser {
     /*@NotNull*/
     private Expression parseDocumentConstructor(int offset) throws XPathException {
         nextToken();
-        Expression content = parseExpression();
+        Expression content;
+        if (t.currentToken == Token.RCURLY && allowXPath31Syntax) {
+            content = Literal.makeEmptySequence();
+        } else {
+            content = parseExpression();
+        }
         expect(Token.RCURLY);
         lookAhead();  // must be done manually after an RCURLY
         nextToken();
@@ -3882,7 +3887,12 @@ public class XQueryParser extends XPathParser {
         }
         int offset = t.currentTokenStartOffset;
         nextToken();
-        Expression tryExpr = parseExpression();
+        Expression tryExpr;
+        if (t.currentToken == Token.RCURLY && allowXPath31Syntax) {
+            tryExpr = Literal.makeEmptySequence();
+        } else {
+            tryExpr = parseExpression();
+        }
         TryCatch tryCatch = new TryCatch(tryExpr);
         setLocation(tryCatch, offset);
         expect(Token.RCURLY);
@@ -3952,7 +3962,12 @@ public class XQueryParser extends XPathParser {
                 test = new UnionQNameTest(tests);
             }
             catchDepth++;
-            Expression catchExpr = parseExpression();
+            Expression catchExpr;
+            if (t.currentToken == Token.RCURLY && allowXPath31Syntax) {
+                catchExpr = Literal.makeEmptySequence();
+            } else {
+                catchExpr = parseExpression();
+            }
             tryCatch.addCatchExpression(test, catchExpr);
             expect(Token.RCURLY);
             lookAhead();  // must be done manually after an RCURLY
