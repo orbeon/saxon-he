16402b64d2cbb280891485e5f074aa4db33b3859
O'Neil Delpratt <oneil@saxonica.com>
2019-01-07 17:26:12 +0000

    Added NullDestination for dotnet

---
+++ latest9.9/hej/net/sf/saxon/dotnet/DotNetNullDestination.java
@@ -0,0 +1,49 @@
+package net.sf.saxon.dotnet;
+
+import net.sf.saxon.event.PipelineConfiguration;
+import net.sf.saxon.event.Receiver;
+import net.sf.saxon.event.Sink;
+import net.sf.saxon.s9api.Action;
+import net.sf.saxon.s9api.Destination;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.serialize.SerializationProperties;
+
+import java.net.URI;
+
+/**
+ * A NullDestination is a Destination that constructs an XmlDocument, the .NET implementation for Null Destination. The getReceiver returns a Sink
+ * which disregards all information passed to it.
+  *  @since 9.9
+ */
+public class DotNetNullDestination implements Destination {
+
+    @Override
+    public void setDestinationBaseURI(URI baseURI) {
+
+    }
+
+    @Override
+    public URI getDestinationBaseURI() {
+        return null;
+    }
+
+    @Override
+    public Receiver getReceiver(PipelineConfiguration pipe, SerializationProperties params) throws SaxonApiException {
+        return new Sink(pipe);
+    }
+
+    @Override
+    public void onClose(Action listener) {
+
+    }
+
+    @Override
+    public void closeAndNotify() throws SaxonApiException {
+
+    }
+
+    @Override
+    public void close() throws SaxonApiException {
+
+    }
+}
