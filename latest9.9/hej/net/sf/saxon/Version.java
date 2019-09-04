8c594abd92e7dd22d3fcdde3e248925ee219f064
O'Neil Delpratt <oneil@saxonica.com>
2019-09-04 11:52:20 +0000

    Saxon Version date updated

--- latest9.9/hej/net/sf/saxon/Version.java
+++ latest9.9/hej/net/sf/saxon/Version.java
@@ -15,8 +15,8 @@ public final class Version {
 
     private static final int[] STRUCTURED_VERSION = {9, 9, 1, 5};
     private static final String VERSION = "9.9.1.5";
-    private static final String BUILD = "090314"; //mmddhh
-    private static final String RELEASE_DATE = "2019-09-03";
+    private static final String BUILD = "090514"; //mmddhh
+    private static final String RELEASE_DATE = "2019-09-05";
     private static final String MAJOR_RELEASE_DATE = "2018-09-27";
 
     private Version() {
