81123b00126bebc44c600bbb74b6cb0237c5cff3
O'Neil Delpratt <oneil@saxonica.com>
2020-03-02 11:03:27 +0000

    Version number changed to 9.9.1.7

--- latest9.9/hej/net/sf/saxon/Version.java
+++ latest9.9/hej/net/sf/saxon/Version.java
@@ -13,10 +13,10 @@ package net.sf.saxon;
 
 public final class Version {
 
-    private static final int[] STRUCTURED_VERSION = {9, 9, 1, 6};
-    private static final String VERSION = "9.9.1.6";
-    private static final String BUILD = "120613"; //mmddhh
-    private static final String RELEASE_DATE = "2019-12-06";
+    private static final int[] STRUCTURED_VERSION = {9, 9, 1, 7};
+    private static final String VERSION = "9.9.1.7";
+    private static final String BUILD = "030315"; //mmddhh
+    private static final String RELEASE_DATE = "2020-03-03";
     private static final String MAJOR_RELEASE_DATE = "2018-09-27";
 
     private Version() {
