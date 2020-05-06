cb0e29df1b4b3f64a18000b77f3a6b9c0179e6ca
O'Neil Delpratt <oneil@saxonica.com>
2020-05-06 11:11:55 +0000

    Update Version of Saxon 10.1

--- latest10/hej/net/sf/saxon/Version.java
+++ latest10/hej/net/sf/saxon/Version.java
@@ -17,7 +17,7 @@ public final class Version {
     private static final int MINOR_VERSION = 0;
     private static final int BUILD = 31609; //mddhh
     private static final String MAJOR_RELEASE_DATE = "2020-03-16";
-    private static final String MINOR_RELEASE_DATE = "2020-03-16";
+    private static final String MINOR_RELEASE_DATE = "2020-05-06";
 
 
     private Version() {
