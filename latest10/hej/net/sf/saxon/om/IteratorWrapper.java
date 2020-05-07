22e7a890f13f2a08e9a037f52460f6249e147f7c
O'Neil Delpratt <oneil@saxonica.com>
2020-05-07 16:58:03 +0000

    Add the IteratorWraper class

---
+++ latest10/hej/net/sf/saxon/om/IteratorWrapper.java
@@ -0,0 +1,34 @@
+package net.sf.saxon.om;
+
+import net.sf.saxon.trans.XPathException;
+import java.util.Iterator;
+
+/**
+ * Class IteratorWrapper - provides an an SequenceIterator over a Java Iterator.
+ */
+public class IteratorWrapper implements SequenceIterator {
+
+    Iterator<? extends Item> iterator;
+
+    /**
+     * Create a IteratorWrapper backed by an iterator
+     * @param iterator the iterator that delivers the items in the sequence
+     */
+
+    public IteratorWrapper(Iterator<? extends Item> iterator) {
+        this.iterator = iterator;
+    }
+
+
+    /**
+     * Get the next item in the Iterator
+     *
+     * @return the next item in the iterator
+     */
+    @Override
+    public Item next() throws XPathException {
+        return iterator.next();
+    }
+}
+
+
