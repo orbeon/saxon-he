d5db19651267e013985ff2efd4026e6ffd11bf10
Michael Kay <mike@saxonica.com>
2020-07-01 11:02:03 +0000

    Fix bug #4610

--- latest9.9/hej/net/sf/saxon/expr/SlashExpression.java
+++ latest9.9/hej/net/sf/saxon/expr/SlashExpression.java
@@ -347,11 +347,14 @@ public class SlashExpression extends BinaryExpression
         Optimizer opt = visitor.obtainOptimizer();
 
         getLhs().optimize(visitor, contextItemType);
+        if (Literal.isEmptySequence(getStart())) {
+            return Literal.makeEmptySequence();
+        }
+
         ContextItemStaticInfo cit = visitor.getConfiguration().makeContextItemStaticInfo(getStart().getItemType(), false);
         cit.setContextSettingExpression(getStart());
         getRhs().optimize(visitor, cit);
-
-        if (Literal.isEmptySequence(getStart()) || Literal.isEmptySequence(getStep())) {
+        if (Literal.isEmptySequence(getStep())) {
             return Literal.makeEmptySequence();
         }
 
