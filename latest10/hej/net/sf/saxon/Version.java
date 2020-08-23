0d7e59586c9dd0ecbb3d656bfe4ecc815fb044a9
O'Neil Delpratt <oneil@saxonica.com>
2020-08-23 08:04:04 +0000

    Updated Version date

--- latest10/hej/net/sf/saxon/Version.java
+++ latest10/hej/net/sf/saxon/Version.java
@@ -15,9 +15,9 @@ public final class Version {
 
     private static final int MAJOR_VERSION = 10;
     private static final int MINOR_VERSION = 2;
-    private static final int BUILD = 81815; //mddhh
+    private static final int BUILD = 82414; //mddhh
     private static final String MAJOR_RELEASE_DATE = "2020-03-16";
-    private static final String MINOR_RELEASE_DATE = "2020-08-18";
+    private static final String MINOR_RELEASE_DATE = "2020-08-24";
 
 
     private Version() {
